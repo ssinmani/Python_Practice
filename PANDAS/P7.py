d60d7409a6f30309e994efd1f25f235e8a3f5fdc
rr1180 <128721849+rr1180@users.noreply.github.com>
2026-09-25 14:44:07 -0400

    Groupby Problems

---
+++ PANDAS/P7.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data ={
+    "Patient": ["P101","P102","P103","P104","P105"],
+    "Age" :[45,32,67,28,54],
+    "Department" :["Cardiology","Neurology","Cardiology","Emergency","Neurology"],
+    "Waiting_Time" :[32,45,63,29,55]
+}
+df =pd.DataFrame(data)
+print("Department Count",df["Department"].value_counts())
+print ("Average Waiting Time of each departmnet ",df.groupby("Department")["Waiting_Time"].mean())
+print ("Average Waiting Time of each departmnet ",df.groupby("Department")["Waiting_Time"].sum())
