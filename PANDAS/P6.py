ea1612a87f8aa530b81152b29b8cf60f54b35d1c
rr1180 <128721849+rr1180@users.noreply.github.com>
2026-09-25 14:19:55 -0400

    Count Unique Value

---
+++ PANDAS/P6.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data ={
+    "Patient": ["P101","P102","P103","P104","P105"],
+    "Age" :[45,32,67,28,54],
+    "Department" :["Cardiology","Neurology","Cardiology","Emergency","Neurology"],
+    "Waiting_Time" :[32,45,63,29,55]
+}
+df =pd.DataFrame(data)
+print("Department Count",df["Department"].value_counts())
+
